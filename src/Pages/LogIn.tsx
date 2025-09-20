959e302ed63bd37eca264cacd275073740d2120b
Amarachi-Codes <etheldorisamarachi@gmail.com>
2025-09-20 21:02:12 +0100

    updated login page

--- src/Pages/LogIn.tsx
+++ src/Pages/LogIn.tsx
@@ -2,9 +2,22 @@
 
 const LogIn = () => {
   return (
-    <div>
-      <p>Login</p>
-    </div>
+    <>
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="flex items-center justify-center border-2 rounded-2xl w-[32rem] p-8 shadow-lg bg-white">
+          <form action="" className="flex flex-col gap-2 p-12 w-full">
+            <h2 className="font-bold text-xl text-center">Login</h2>
+            <label htmlFor="">Email</label>
+            <input type="email" name="" id="" className="rounded p-2 border-1" />
+            <label htmlFor="">Password</label>
+            <input type="password" name="" id="" className="rounded p-2 border-1" />
+            <div className=" flex items-center justify-center">
+            <button className="border-1 my-4 bg-indigo-700 rounded text-white p-2">Submit</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/Pages/Register.tsx
+++ src/Pages/Register.tsx
@@ -1,22 +1,48 @@
 const Register = () => {
   return (
     <>
-    <div className="flex flex-col items-center border-2 w-2xl justify-center rounded-2xl ">
-      
-      <div className="flex flex-col p-16 w-full gap-2">
-        <h2 className="text-center font-bold">Register</h2>
-        <label htmlFor="">Enter your name</label>
-        <input type="text" name="" id="" className="border-1 rounded p-1"/>
-        <label htmlFor="">House Address</label>
-        <input type="text" name="" id="" className="border-1  rounded p-1" />
-        <label htmlFor="">Email</label>
-        <input type="email" name="" id="" className="border-1  rounded p-1" />
-        <label htmlFor="">Telephone</label>
-        <input type="text" name="" id="" className="border-1  rounded p-1"/>
-        <label htmlFor="">Resident ID Number</label>
-        <input type="text" name="" id="" className="border-1  rounded p-1" />
+      <div className="flex items-center min-h-screen  justify-center bg-gray-100 ">
+        <div className="flex items-center justify-center rounded-2xl p-10 border-2 w-[32rem] bg-white shadow-lg">
+          <form className="flex flex-col p-6  w-full gap-2">
+            <h2 className="text-center font-bold text-xl mb-4">Register</h2>
+            <label htmlFor="">Enter your name</label>
+            <input type="text" name="" id="" className="border-1 rounded p-2" />
+            <label htmlFor="">House Address</label>
+            <input
+              type="text"
+              name=""
+              id=""
+              className="border-1  rounded p-2"
+            />
+            <label htmlFor="">Email</label>
+            <input
+              type="email"
+              name=""
+              id=""
+              className="border-1  rounded p-2"
+            />
+            <label htmlFor="">Telephone</label>
+            <input
+              type="text"
+              name=""
+              id=""
+              className="border-1  rounded p-2"
+            />
+            <label htmlFor="">Resident ID Number</label>
+            <input
+              type="text"
+              name=""
+              id=""
+              className="border-1  rounded p-2"
+            />
+            <div className=" flex items-center justify-center">
+              <button className="border-1 my-4 bg-indigo-700 rounded text-white p-2">
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
     </>
   );
 };
